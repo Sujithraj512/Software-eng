6136138c38c4887215e34934d09388c41c81e0e3
Sujithraj512 <152624505+Sujithraj512@users.noreply.github.com>
2023-12-04 22:45:51 -0600

    First Commit

---
+++ lachateaurestaurant.wordpress.com/wp-content/plugins/ie-sitemode/custom-jumplist.php
@@ -0,0 +1,2 @@
+function wpcom_custom_jumplist_reader(){window.external.msSiteModeCreateJumplist("Recent posts");window.external.msSiteModeAddJumpListItem("Hello World!","https://lachateaurestaurant.wordpress.com/2023/11/25/hello-world/","https://lachateaurestaurant.files.wordpress.com/2023/11/cropped-logo.png?w=16");}
+try{if(window.external && 'msIsSiteMode' in window.external){if (window.external.msIsSiteMode()){wpcom_custom_jumplist_reader();wpcom_sitemode_comment_notify();}}}catch(e){}
